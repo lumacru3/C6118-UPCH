a50ebb41e6fe49b30424f37cf88b5e1a967a6c47
Lucía Maldonado <lumacru3@gmail.com>
2017-07-08 00:25:34 -0500

    Creando hov y arreglo meses

--- tercer_examen/prueba1.R
+++ tercer_examen/prueba1.R
@@ -2,25 +2,58 @@ library(ncdf4)
 library(fields)
 library(kali)
 
-data_sst = nc_open("datos/sst.nc4")
+data_sst = nc_open("datos/sst.nc4") #abrir archivo de temperatura
 
 #Tamaño time= 552 desde 1959 en meses hasta el año 2005
 #hallar 12 meses del año 1990
 #30 años desde 1959 hasta 1989. sumar 360 meses pero inicia de 0 así que 359
-#360=enero, 372=diciembre de 1990
-
-###anomalias temperatura
+#360=enero, 371=diciembre de 1990
 
 sst = ncvar_get(data_sst, "to")
 lat = ncvar_get(data_sst, "latitude")
 lon = ncvar_get(data_sst, "longitude") - 360
+time = ncvar_get(data_sst, "time")
+solo1990 = time[361:372]
+prof = ncvar_get(data_sst, "zt_ocean") # solo tiene un valor. Profundidad = 5 metros
+
+#Crear arreglo para meses
+meses = rep(1:12, length=length(solo1990))
+
+sst[,, meses==1]
+apply(sst[,, meses==1], MARGIN=c(1,2), FUN=mean, na.rm=TRUE)
+clim= array(dim=c(dim(sst)[1:2], 12)) 
+
+for (i in 1:12){
+  clim[,,i] = apply(sst[,, meses==i], MARGIN=c(1,2), FUN=mean, na.rm=TRUE)
+}
+
+image.map(lon, lat, clim[,,1]) #promedio mes de enero
+title("Promedio T° Enero")
+
+#indexando directamente
 
 sst_promedio = apply(sst[,,1:552], MARGIN = c(1,2), FUN = mean, na.rm = TRUE)
-#image.map(lon, lat, sst_promedio)
+image.map(lon, lat, sst_promedio)
 
-prom_sst_1990 = apply(sst[,,360:372], MARGIN = c(1,2), FUN = mean, na.rm = TRUE)
+prom_sst_1990 = apply(sst[,,361:372], MARGIN = c(1,2), FUN = mean, na.rm = TRUE)
 anom_sst = sst_promedio - prom_sst_1990
 image.map(lon, lat, anom_sst)
+title("Anomalías T° 1990")
+
+##posible hov
+
+par(mfrow=c(1,1))
+hov1 = apply(sst[,,361:372], MARGIN = c(2,3), FUN = mean, na.rm = TRUE) #promedio SST en todo el tiempo
+
+Hovmuller1=image.plot(1:12, lat, t(hov1), las=1,ylim=c(-20,0), xlab="Tiempo", ylab="Latitud", axes=FALSE)
+
+title("Variación de temperatura a lo largo de 1990")
+ejey = paste(abs(axTicks(2)), ifelse(axTicks(2)>0, "°N", "°S"), sep="") 
+axis(2, at=axTicks(2), labels=ejey, las=1) 
+
+points(-5,1) #truco para retomar el foco en el eje, se buscará una mejor solución
+axis(1, at=1:12, labels=month.abb) #agregar meses en eje x
+
 
 ###anomalias salinidad
 
@@ -75,10 +108,13 @@ prom_sphy_invierno = apply(sphy[,,366:368], MARGIN = c(1,2), FUN = mean, na.rm =
 #image.map(lon, lat, anom_sphy)
 image.map(lon, lat, prom_sphy_1990) 
 
-image.map(lon, lat, prom_sphy_verano) 
-image.map(lon, lat, prom_sphy_invierno) 
+image.map(lon, lat, prom_sphy_verano, main="Promedio Fitoplancton verano")
+
+image.map(lon, lat, prom_sphy_invierno,  main="Promedio Fitoplancton invierno") 
 
 par(mfrow=c(2,1), mar=c(0,0,0,0), oma=c(5,5,3,3))
 
 image.map(lon, lat, prom_sphy_verano) 
 image.map(lon, lat, prom_sphy_invierno) 
+
+image.plot(lat, sphy[,,366:368]) 
